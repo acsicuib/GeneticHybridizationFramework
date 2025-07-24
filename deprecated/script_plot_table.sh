58f092ae3edd7dfc69cffa115f04b3d2792c9553
Isaac.Lera <isaac.lera@gmail.com>
2025-07-24 11:26:21 +0200

    Remove plots and results_imperium from tracking; now ignored by .gitignore

--- deprecated/script_plot_table.sh
+++
@@ -1,6 +0,0 @@
-source script_constants.sh
-
-python3 pd_tables.py \
-    'data/analysis/ntw_722_050-050-025_C/obj_distance-occ_variance-pw_consumption/Replicas050/Genetics/hybrid_NSGA2-NSGA3-UNSGA3-SMSEMOA/table.csv' \
-    'data/analysis/ntw_722_050-050-025_C/obj_distance-occ_variance-pw_consumption/Replicas050/Genetics/hybrid_NSGA2-NSGA3-UNSGA3-SMSEMOA/table_no_dumping.csv' \
-    1 ${HYBRID_GEN_STEPS[*]} $HYBRID_N_GEN
